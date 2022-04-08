0fdeaff41d884d5e68632033ec69b2c6e035e3bd
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2022-04-08 13:33:48 +0200

    Add project files.

---
+++ MintPlayer.Extensions.OrderBy/Exceptions/InvalidSortPropertyException.cs
@@ -0,0 +1,9 @@
+﻿namespace MintPlayer.Extensions.OrderBy
+{
+    public class InvalidSortPropertyException : Exception
+    {
+        public InvalidSortPropertyException() : base("The specified sorting property does not exist.")
+        {
+        }
+    }
+}
